ed6061c148e711354378b22fda6447319b70d4d6
hexie2108 <blue.wolf4444444@gmail.com>
2020-06-15 10:30:12 +0200

    v2 版本

--- public/js/get-user.js
+++ public/js/get-user.js
@@ -3,62 +3,11 @@ const BACKEND_URL = document.location.href + 'dynamic/';
 const BILIBILI_URL = 'bilibili.com';
 
 
-//背景颜色类名列表
-const BG_CLASSES = [
-    'badge-secondary',
-    'badge-primary',
-    'badge-success',
-    'badge-info',
-    'badge-warning',
-    'badge-danger',
-];
-//职业名列表
-const CAREERS = [
-    {
-        name: '骑士',
-        range: {
-            attackMin: 25,
-            attackMax: 35,
-            defendMin: 10,
-            defendMax: 15,
-            dodgeMin: 0,
-            dodgeMax: 10,
-            mortalityMin: 0,
-            mortalityMax: 5,
-        }
-    },
-    {
-        name: '枪兵',
-        range: {
-            attackMin: 30,
-            attackMax: 40,
-            defendMin: 5,
-            defendMax: 10,
-            dodgeMin: 5,
-            dodgeMax: 15,
-            mortalityMin: 5,
-            mortalityMax: 10,
-        }
-    },
-    {
-        name: '弓手',
-        range: {
-            attackMin: 25,
-            attackMax: 30,
-            defendMin: 0,
-            defendMax: 5,
-            dodgeMin: 10,
-            dodgeMax: 30,
-            mortalityMin: 10,
-            mortalityMax: 25,
-        }
-    }
-];
-
-
 $(function () {
 
-    $('#get-button').on('click', getRepostUser);
+    $('.get-button').on('click', getRepostUser);
+
+
 
 });
 
@@ -74,49 +23,51 @@ function getRepostUser(event) {
     let $linkInput = $('#dynamic_link');
     let $inputErrorText = $('#dynamic_link_error_text');
 
+    let $toast = $('.toast1');
+
+
     let dynamicLink = $.trim($linkInput.val());
     //如果是空值
     if (!dynamicLink) {
         $linkInput.addClass('is-invalid ');
-        $inputErrorText.html("链接为空");
-        $inputErrorText.fadeIn('slow');
+        $toast.children('.toast-body').html("链接为空");
+        $toast.toast('show');
     }
     //如果不是哔哩哔哩
     else if (!dynamicLink.includes(BILIBILI_URL)) {
         $linkInput.addClass('is-invalid ');
-        $inputErrorText.html("链接不是哔哩哔哩的");
-        $inputErrorText.fadeIn('slow');
+        $toast.children('.toast-body').html("链接不属于哔哩哔哩");
+        $toast.toast('show');
     }
     else {
 
-
         //移除错误提示
         $linkInput.removeClass('is-invalid ');
-        $inputErrorText.fadeOut('slow');
 
         //提取出 动态id
+        let dynamicId;
         let index = dynamicLink.lastIndexOf("/");
         if (index !== -1) {
-            dynamicLink = dynamicLink.slice(index + 1);
+            dynamicId = dynamicLink.slice(index + 1);
         }
-        index = dynamicLink.indexOf("?");
+        index = dynamicId.indexOf("?");
         if (index !== -1) {
-            dynamicLink = dynamicLink.slice(0, index);
+            dynamicId = dynamicId.slice(0, index);
         }
-        index = dynamicLink.indexOf("#");
+        index = dynamicId.indexOf("#");
         if (index !== -1) {
-            dynamicLink = dynamicLink.slice(0, index);
+            dynamicId = dynamicId.slice(0, index);
         }
 
         //如果地址错误
-        if (isNaN(dynamicLink)) {
+        if (isNaN(dynamicId)) {
             $linkInput.addClass('is-invalid ');
-            $inputErrorText.html("动态地址填写错误");
-            $inputErrorText.fadeIn('slow');
+            $toast.children('.toast-body').html("动态链接不正确");
+            $toast.toast('show');
         }
         else {
 
-            sendRequest(dynamicLink);
+            sendRequest(dynamicId);
         }
 
     }
@@ -130,19 +81,21 @@ function getRepostUser(event) {
  */
 function sendRequest(dynamicId) {
 
-    //注销按钮
-    $('#get-button').attr('disabled', true);
-
-    console.log("获取动态ID " + dynamicId)
-
+    let $toast = $('.toast1');
     //列表
     let $userListElement = $('.user-list');
     //清空列表
     $userListElement.empty();
 
-    let $loading = $('#loading');
-    //现实进度条
-    $loading.fadeIn('slow');
+    //清空数据列表
+    USER_LIST.length = 0;
+
+    //注销按钮
+    $('#get-button').attr('disabled', true);
+    //显示模态加载窗口
+    $('.modal').modal('show');
+
+    console.log("获取动态ID " + dynamicId)
 
 
     $.getJSON(BACKEND_URL + dynamicId, onSuccess).fail(onError).always(onComplete);
@@ -158,74 +111,53 @@ function sendRequest(dynamicId) {
             //遍历每个元素
             data.forEach(function (user) {
 
-                //添加创建额外数值
-                user = Object.assign(user, generateCareer(user));
                 //保存到全局列表
                 USER_LIST.push(user);
 
                 //创建插入元素到页面
                 let newElement = `
-<li class='list-group-item' id="user-${user.uid}">
-    <img class="img-fluid rounded-circle " src="${user.avatar}" alt="${user.name}" referrerPolicy="no-referrer"/>
+<div class="border col-3 m-2 rounded">
+    <img class="img-fluid rounded-circle m-1" src="${user.avatar}" alt="${user.name}" referrerPolicy="no-referrer"/>
     <span class="m-1 name small">
         ${user.name}
     </span>
     <span class="badge ${getBgColorClass(user.level - 1)} m-1">
         Lv ${user.level}
     </span>
-    <span class="badge ${getBgColorClass(user.career)} m-1">
-    ${getCareerName(user.career)}
-    </span>
-    
-</li>`;
+</div>`;
                 $userListElement.append(newElement);
             });
 
+            // 激活按钮
+            $('.start-button').removeAttr('disabled');
 
-            $('#start-button').removeAttr('disabled');
-            $('#stop-button').removeAttr('disabled');
-            $('#speed-up-button').removeAttr('disabled');
 
         }
 
     }
 
     function onEmptyError() {
-        alert("转发用户列表为空!");
+
+        $('.toast .toast-body').html("转发用户列表为空");
+        $toast.toast('show');
     }
 
     function onError() {
-        alert("获取错误, 请检查后台服务!");
+
+        $('.toast .toast-body').html("获取错误, 请检查后台服务!");
+        $toast.toast('show');
     }
 
     function onComplete() {
-        //隐藏进度条
-        $loading.fadeOut('slow');
+        //重新激活按钮
+        $('.get-button').removeAttr('disabled');
+        //隐藏模态加载窗口
+        $('.modal').modal('hide');
     }
 
 
 }
 
-/**
- * 设置职业id, 0 = 骑士, 1 = 枪兵, 2 = 射手
- * @return {number}
- */
-function generateCareer(user) {
-
-    let gameData = {};
-
-    //随机职业ID
-    let careerIndex = getRandomInt(0, CAREERS.length - 1);
-    gameData.career = careerIndex;
-    gameData.hp = 100;
-    gameData.attack = getRandomInt(CAREERS[careerIndex].range.attackMin + user.level * 3, CAREERS[careerIndex].range.attackMax + user.level * 3);
-    gameData.defend = getRandomInt(CAREERS[careerIndex].range.defendMin + user.level * 3, CAREERS[careerIndex].range.defendMax + user.level * 3);
-    gameData.dodge = getRandomInt(CAREERS[careerIndex].range.dodgeMin + user.level * 1, CAREERS[careerIndex].range.dodgeMax + user.level * 2);
-    gameData.mortality = getRandomInt(CAREERS[careerIndex].range.mortalityMin + user.level * 2, CAREERS[careerIndex].range.mortalityMax + user.level * 2);
-
-    return gameData;
-
-}
 
 
 /**
@@ -245,6 +177,16 @@ function getCareerName(careerIndex) {
  * @return string css类名
  */
 function getBgColorClass(index) {
+
+    //背景颜色类名列表
+    const BG_CLASSES = [
+        'badge-secondary',
+        'badge-primary',
+        'badge-success',
+        'badge-info',
+        'badge-warning',
+        'badge-danger',
+    ];
 
     //避免index超过范围
     index = index % BG_CLASSES.length;
--- public/js/start-game.js
+++ public/js/start-game.js
@@ -1,50 +1,25 @@
-var leftPlayer;
-var rightPlayer;
+let winnerNumber;
+const SPEED = 400;
 
-//计时
-var time = 0;
-//时间周期管理器
-var timeInterval;
-//攻击周期暂停
-var stopAttack = false;
-
-var actionInterval = 500;
-
-var leftFirstAttack;
 
 $(function () {
 
-    $('#start-button').on('click', function (event) {
+    $('.start-button').on('click', function (event) {
 
         event.preventDefault();
         //注销按钮
         $(event.target).attr('disabled', true);
 
-        //开启计时, 每秒更新时间
-        timeInterval = setInterval(updateTime, 1000);
-        //重置暂停
-        stopAttack = false;
+        //获取抽选人数
+        winnerNumber = $('#winner-number').val();
+
+        $('.toast2 .toast-title').html("抽选进行中... ");
+        $('.toast2').toast('show');
 
         startGame();
+
     });
 
-    $('#stop-button').on('click', function (event) {
-        event.preventDefault();
-        //中断攻击
-        stopAttack = true;
-        //中断计时
-        clearInterval(timeInterval);
-        //重选激活开始按钮
-        $('#start-button').removeAttr('disabled');
-
-    })
-
-    //加速按钮
-    $('#speed-up-button').on('click', function (event) {
-        event.preventDefault();
-        //减少等待时间
-        actionInterval -= 100;
-    });
 
 });
 
@@ -55,260 +30,65 @@ $(function () {
 function startGame() {
 
 
-    let $leftPlayerElement = $('.main .left-player');
-    let $rightPlayerElement = $('.main .right-player');
+    //如果剩余人数大于要抽选的人数, 继续抽选
+    if (USER_LIST.length > winnerNumber) {
 
-    //如果未填充或 已阵亡
-    if ($leftPlayerElement.find('.avatar img:not(.death)').length === 0) {
-        selectNewPlayer($leftPlayerElement, true);
-    }
-    //如果未填充或 已阵亡
-    if ($rightPlayerElement.find('.avatar img:not(.death)').length === 0) {
-        selectNewPlayer($rightPlayerElement, false);
-    }
+        //随机生成index
+        let randomIndex = getRandomInt(0, USER_LIST.length - 1);
 
-    //重置生命
-    changeHp(leftPlayer, $leftPlayerElement, 100);
-    changeHp(rightPlayer, $rightPlayerElement, 100);
-
-    //显示主体
-    $('.main').fadeIn('fast');
-
-    //更新用户数
-    $('.user-number').html('剩余用户数: ' + USER_LIST.length);
-
-    //初始化攻击先手
-    //生成 0~1
-    let randomNumber = getRandomInt(0, 1);
-    leftFirstAttack = randomNumber === 1;
-
-    //定时1秒后开始攻击
-    setTimeout(function () {
-        attack();
-    }, actionInterval * 5);
-}
+        //从页面列表隐藏元素, 然后运行回调函数
+        $('.user-list div').eq(randomIndex).fadeOut(removeSpeed(), function () {
+            //从页面移除元素
+            $(this).remove();
+            //从全局INDEX数组中移除
+            USER_LIST.splice(randomIndex, 1);
+            //更新剩余数量显示
+            $('.toast2 .toast-body').html("剩余: " + USER_LIST.length);
+            //定时下个循环
+            setTimeout(startGame, removeSpeed());
+        });
 
 
-/**
- * 填充新玩家
- * @param {Object} $playerElement 玩家主体元素
- * @param {boolean} isLeft 是否是左边玩家
- */
-function selectNewPlayer($playerElement, isLeft) {
+    } else {
 
-	//随机选择
-    //let randomIndex = getRandomInt(0, USER_LIST.length - 1);
-	//选择第一个
-	let randomIndex = 0;
- 
-    //提取被选中用户
-    let user = USER_LIST[randomIndex];
-
-    //根据方向, 把用户存储到全局变量中
-    if (isLeft) {
-        leftPlayer = user;
-    }
-    else {
-        rightPlayer = user
+        endGame();
     }
 
-    //从全局数组中移除
-    USER_LIST.splice(randomIndex, 1);
-    //从页面列表移除
-    $('.user-list li').eq(randomIndex).remove();
-
-    //创建图片元素
-    let avatar = `<img src="${user.avatar}" class="img-fluid rounded-circle" alt=""/>`;
-    //创建等级元素
-    let badgeLevel = `
-    <span class="badge ${getBgColorClass(user.level - 1)}">Lv ${user.level}</span>`;
-    let badgeCareer = `
-    <span class="badge ${getBgColorClass(user.career)}">   ${getCareerName(user.career)}</span>
-`;
-
-    //更新玩家显示数据
-    $playerElement.find('.name').html(user.name);
-    $playerElement.find('.avatar').empty().append(avatar);
-    $playerElement.find('.attack').html(user.attack);
-    $playerElement.find('.defend').html(user.defend);
-    $playerElement.find('.dodge').html(user.dodge);
-    $playerElement.find('.mortality').html(user.mortality);
-
-    $playerElement.find('.badge-level').empty().append(badgeLevel);
-    $playerElement.find('.badge-career').empty().append(badgeCareer);
-
-    printMessage(`"<b class="text-info">${user.name}</b>" 进入战场`);
 
 }
 
 /**
- * 进行攻击动作
+ * 结束游戏
  */
-function attack() {
-
-    //如果是默认未暂停状态
-    if (!stopAttack) {
-
-        let attacker, defender, $attackElement, $defenderElement, moveAnimationStart, moveAnimationEnd;
+function endGame() {
 
 
-        if (leftFirstAttack) {
-            attacker = leftPlayer;
-            defender = rightPlayer;
-            $attackElement = $('.main .left-player');
-            $defenderElement = $('.main .right-player');
-            moveAnimationStart = {marginLeft: '200px'};
-            moveAnimationEnd = {marginLeft: '0px'};
-        }
-        else {
-            attacker = rightPlayer;
-            defender = leftPlayer;
-            $attackElement = $('.main .right-player');
-            $defenderElement = $('.main .left-player');
-            moveAnimationStart = {marginRight: '100px'};
-            moveAnimationEnd = {marginRight: '0px'};
-        }
+    let winnerName = '';
 
-
-        printMessage(`"<b class="text-info">${attacker.name}</b>" 发动了攻击`);
-
-        //攻击动画
-        $attackElement.find('img').animate(moveAnimationEnd, actionInterval, function () {
-            //插入攻击音效
-            $('audio.attack').eq(attacker.career).trigger('play');
-        }).animate(moveAnimationStart, actionInterval / 2).animate(moveAnimationEnd, actionInterval / 2);
-
-
-        //手动延时
-        setTimeout(function () {
-
-            //闪避判断, 随机命中 大于 闪避值 说明攻击成功
-            if (getRandomInt(0, 100) > defender.dodge) {
-
-                //伤害判断
-                let damage = attacker.attack - defender.defend;
-                //如果不破防 强制扣血
-                if (damage <= 0) {
-                    damage = 1;
-                }
-                //如果有职业相克 双倍伤害,  骑士 克 弓手, 枪兵 克 骑士, 弓手 克 骑士
-                if (attacker.career === (defender.career + 1) % CAREERS.length) {
-                    damage *= 2;
-                }
-
-                //必杀伤害 加倍
-                if (getRandomInt(0, 100) < attacker.mortality) {
-                    damage *= 2;
-                    printMessage(`<p class="text-danger">"${attacker.name}" 使出了必杀一击, 对 "${defender.name}" 造成了 <b>${damage}</b> 点 必杀伤害</p>`);
-                }
-                else {
-                    printMessage(`"<b class="text-info">${defender.name}</b>" 受到了 <b class="text-danger">${damage}</b> 点 伤害`);
-                }
-
-
-                //受伤害特效
-                $defenderElement.find('img').fadeOut(actionInterval / 5).fadeIn(actionInterval / 5).fadeOut(actionInterval / 5).fadeIn(actionInterval / 5);
-
-                //血量更新
-                changeHp(defender, $defenderElement, -damage);
-
-                //如果防守方死亡
-                if (defender.hp === 0) {
-
-                    //头像变黑白
-                    $defenderElement.find('img').addClass('death').css('filter', 'grayscale(1)');
-                    //插入死亡音效
-                    $('audio.death').eq(getRandomInt(0, 1)).trigger('play');
-                    //1秒后退出通知
-                    setTimeout(function () {
-                        printMessage(`"<b class="text-info">${defender.name}</b>" 已退出战场`);
-                    }, actionInterval * 2);
-
-
-                    //2秒后重选开始新游戏
-                    setTimeout(function () {
-                        //还有其他用户
-                        if (USER_LIST.length > 0) {
-                            startGame(attacker);
-                        }
-                        else {
-                            win(attacker);
-                        }
-                    }, actionInterval * 5);
-
-                }
-                else {
-                    //插入受伤音效
-                    $('audio.hurt').eq(getRandomInt(0, 2)).trigger('play');
-                    //等待下一轮攻击
-                    setTimeout(attack, actionInterval * 2);
-                }
-            }
-            else {
-
-                //闪避动画
-                $defenderElement.find('img').animate(moveAnimationStart, actionInterval / 2).animate(moveAnimationEnd, actionInterval / 2);
-
-                printMessage(`攻击未命中`);
-                //等待下一轮攻击
-                setTimeout(attack, actionInterval * 2);
-            }
-
-            //下轮让对方攻击
-            leftFirstAttack = !leftFirstAttack;
-        }, actionInterval * 2)
+    for (let i = 0; i < USER_LIST.length; i++) {
+        winnerName += USER_LIST[i].name + " , ";
     }
 
+    console.log("抽奖已结束");
+
+    $('.toast2 .toast-title').html("抽选已结束");
+    $('.toast2 .toast-body').html("恭喜中奖者: " + winnerName);
 }
 
 /**
- * 输出胜利者信息
- * @param {Object}winner
+ * 根据列表长度动态设置速度
+ * @returns {number}
  */
-function win(winner) {
-    let $text = `胜利者出现了~ 恭喜 "<b class="text-danger">${winner.name}</b>" 获得了最终的胜利, 稍后将会私信联系您</b>`;
-    printMessage($text);
-    //暂停时间计时
-    clearInterval(timeInterval);
-}
+function removeSpeed() {
 
-/**
- * 输出消息信息
- * @param {string} text
- */
-function printMessage(text) {
-    $('.message').html(text);
+    let speedRation = USER_LIST.length / 10;
 
-}
-
-/**
- * 设置生命值
- * @param {Object}player
- * @param {Object}$playerElement
- * @param value
- */
-function changeHp(player, $playerElement, value) {
-
-    //更新血量
-    player.hp += value;
-    //避免出现负数 或超过上限
-    if (player.hp < 0) {
-        player.hp = 0
+    if (speedRation > 1) {
+        return SPEED / speedRation;
+    } else {
+        return SPEED;
     }
-    if (player.hp > 100) {
-        player.hp = 100;
-    }
-    //更新血量条
-    $playerElement.find('.hp').css('width', player.hp + '%').html(player.hp);
-}
 
 
-/**
- * 计算游戏时间
- */
-function updateTime() {
-
-    $('.time').html('游戏时间: ' + time + " 秒");
-    time++;
 }
 
